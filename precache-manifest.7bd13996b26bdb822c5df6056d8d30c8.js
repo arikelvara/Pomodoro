28969ddc02e2efb3085c1c3c3bead752ca0c46cc
arikelvara <arikmehta@users.noreply.github.com>
2020-06-27 20:01:02 +1000

    Updates

---
+++ precache-manifest.7bd13996b26bdb822c5df6056d8d30c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "428f23abc80c74f3a313d7224c97b78f",
+    "url": "/Pomodoro.git/index.html"
+  },
+  {
+    "revision": "492db792cd8f4800dca9",
+    "url": "/Pomodoro.git/static/css/main.3a52fd4b.chunk.css"
+  },
+  {
+    "revision": "a06900e632f2217ad43d",
+    "url": "/Pomodoro.git/static/js/2.e59ed367.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pomodoro.git/static/js/2.e59ed367.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "492db792cd8f4800dca9",
+    "url": "/Pomodoro.git/static/js/main.7b84eb3e.chunk.js"
+  },
+  {
+    "revision": "678c33f7216f99017006",
+    "url": "/Pomodoro.git/static/js/runtime-main.55415aa5.js"
+  }
+]);
